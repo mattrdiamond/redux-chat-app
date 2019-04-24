5c568c5e7bea39bd0fd170b389fa6b89c658acf6
Matt Diamond <mattdiamond80@gmail.com>
2019-04-24 16:53:02 -0500

    Add active state to active user within sidebar

--- src/containers/User.js
+++ src/containers/User.js
@@ -3,10 +3,14 @@ import { setActiveUserId } from "../actions";
 import "./User.css";
 import { connect } from "react-redux";
 
-const User = ({ user, handleUserClick }) => {
+const User = ({ user, handleUserClick, activeUserId }) => {
   const { name, profile_pic, status, user_id } = user;
+  console.log("active id", activeUserId);
   return (
-    <div className="User" onClick={handleUserClick.bind(null, user_id)}>
+    <div
+      className={"User" + (user_id === activeUserId ? " active" : "")}
+      onClick={handleUserClick.bind(null, user_id)}
+    >
       <img src={profile_pic} alt={name} className="User__pic" />
       <div className="User__details">
         <p className="User details-name">{name}</p>
@@ -14,6 +18,13 @@ const User = ({ user, handleUserClick }) => {
       </div>
     </div>
   );
+};
+
+const mapStateToProps = state => {
+  const { activeUserId } = state;
+  return {
+    activeUserId: activeUserId
+  };
 };
 
 const mapDispatchToProps = dispatch => {
@@ -25,6 +36,32 @@ const mapDispatchToProps = dispatch => {
 };
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(User);
+
+// const User = ({ user, handleUserClick }) => {
+//   const { name, profile_pic, status, user_id } = user;
+//   return (
+//     <div className="User" onClick={handleUserClick.bind(null, user_id)}>
+//       <img src={profile_pic} alt={name} className="User__pic" />
+//       <div className="User__details">
+//         <p className="User details-name">{name}</p>
+//         <p className="User__details-status">{status}</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     handleUserClick: user_id => {
+//       dispatch(setActiveUserId(user_id));
+//     }
+//   };
+// };
+
+// export default connect(
+//   null,
+//   mapDispatchToProps
+// )(User);
