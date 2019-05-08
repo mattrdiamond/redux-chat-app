9354ec72a86969842c0696e882d505b4cee6b82a
Matt Diamond <mattdiamond80@gmail.com>
2019-05-08 17:36:58 -0500

    New layout for empty screen

--- src/components/Empty.js
+++ src/components/Empty.js
@@ -3,22 +3,25 @@ import "./Empty.css";
 
 const Empty = ({ user }) => {
   const { name, profile_pic, status } = user;
-  const removePrefix = name.replace(/\b(dr|mr|mrs|ms)[.\s]\s?/gi, "");
+  const removePrefix = name.replace(/\b(dr|mr|mrs|ms|miss)[.\s]\s?/gi, "");
   const firstName = removePrefix.split(" ")[0];
 
   return (
     <div className="Empty">
-      <h1 className="Empty__name">Welecome, {firstName}</h1>
-      <img src={profile_pic} alt={name} className="Empty__img" />
-      <p className="Empty__status">
-        <b>Status: </b>
-        {status}
-      </p>
-      <button className="Empty__btn">Start a conversation</button>
-      <p className="Empty__info">
-        Search for someone to start chatting with or go to Contacts to see who
-        is available
-      </p>
+      <div className="Empty__content">
+        <img src={profile_pic} alt={name} className="Empty__img" />
+        <h1 className="Empty__name">Welcome, {firstName}.</h1>
+        <p className="Empty__info">
+          Search for someone to start chatting with or go to Contacts to see who
+          is available.
+        </p>
+      </div>
+      <div className="Empty__status-container">
+        <p className="Empty__status">
+          <b>Status: </b>
+          {status}
+        </p>
+      </div>
     </div>
   );
 };
