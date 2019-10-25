a2b970eaa57ee4eb031626ed78f4c2f1d2bacfc7
Matt Diamond <mattdiamond80@gmail.com>
2019-10-25 14:25:00 -0500

    Updates

---
+++ precache-manifest.c11bcbc67e48c19b34a195445de8feb1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "457e94a5b665614917d1dc45a13d97b8",
+    "url": "/chatbox/static/media/avenir-next-regular.457e94a5.woff2"
+  },
+  {
+    "revision": "0a500c057dd88e9fcbca",
+    "url": "/chatbox/static/css/main.cdadaa48.chunk.css"
+  },
+  {
+    "revision": "a01ba82625e6090450b5",
+    "url": "/chatbox/static/js/runtime~main.7c1536e7.js"
+  },
+  {
+    "revision": "ec66508700c83867f2d6",
+    "url": "/chatbox/static/js/2.8dfb0d28.chunk.js"
+  },
+  {
+    "revision": "f3279efc748b394d7b81a8d519f0dfe9",
+    "url": "/chatbox/static/media/icons.f3279efc.svg"
+  },
+  {
+    "revision": "0a500c057dd88e9fcbca",
+    "url": "/chatbox/static/js/main.e30fa8d7.chunk.js"
+  },
+  {
+    "revision": "d42bfc911f5d68722bc0e2bb0f0e46d3",
+    "url": "/chatbox/static/media/avenir-next-demi.d42bfc91.woff2"
+  },
+  {
+    "revision": "a17c827d23b2077f3be3824965774bba",
+    "url": "/chatbox/static/media/avenir-next-regular.a17c827d.woff"
+  },
+  {
+    "revision": "7d6afb9bfc9ae3dda53b3b8feb59c684",
+    "url": "/chatbox/static/media/avenir-next-demi.7d6afb9b.woff"
+  },
+  {
+    "revision": "772e67c1a2cfbb14fc0db95a048e1972",
+    "url": "/chatbox/static/media/search.772e67c1.svg"
+  },
+  {
+    "revision": "7ed3087bb9a901335047288938a5702e",
+    "url": "/chatbox/index.html"
+  }
+];
